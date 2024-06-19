7eef5d8c52170059e2dd653b0e77e725b94d37cf
Merens-Y <58517637+Merens-Y@users.noreply.github.com>
2024-06-18 23:29:46 -0400

    Added context menu to machine card component

--- src/lib/components/MachineCards/MoldingMachine.svelte
+++ src/lib/components/MachineCards/MoldingMachine.svelte
@@ -1,23 +1,26 @@
 <!-- DOCUMENT: Create appropiate wiki page only after setting proper types and props. -->
-<!-- TODO: Add context menu -->
 <!-- TODO: Add machine name edit button -->
 <!-- TODO: Give better styling to tooltips for each widget -->
 <!-- TODO: Add machine serial number to machine name if custom name is not available. Then show it as a subtitle if it is.  -->
 <script lang="ts">
+	// Translation function
 	import { LL } from '$lib/i18n/i18n-svelte';
-    import CyclePerformanceIndicator from './ui/CyclePerformanceIndicator.svelte';
+	// Shadcn-Svelte ui elements
+	import * as ContextMenu from '$lib/components/ui/context-menu';
+	// Custom Machine Card widget elements
+	import CyclePerformanceIndicator from './ui/CyclePerformanceIndicator.svelte';
 	import MachineHealthBar from './ui/MachineHealthBar.svelte';
-    import StatusLed from './ui/StatusLED.svelte';
+	import StatusLed from './ui/StatusLED.svelte';
 	import StatusTag from './ui/StatusTag.svelte';
-	
+
 	export let moldingMachineProps = {
 		id: 'molding001',
-		customName: '',
+		customName: ''
 	};
 	// function that will be called when the button is clicked, it will loop through ledProps and then flip the booleans.
 	function randomizeMachineProps() {
 		return;
-	};
+	}
 
 	const tagProps = {
 		isAuto: true,
@@ -40,14 +43,30 @@
 	};
 </script>
 
-<div class="max-w-fit flex-col select-none cursor-default rounded-lg border bg-card text-card-foreground gap-4 m-2 p-4 shadow sm:w-[350px] md:w-[400px] md:shadow-lg lg:w-[450px]">
-	<div class="flex w-full gap-x-1 mb-4 h-auto">
-		<div class="flex-1 grow text-md uppercase">{moldingMachineProps.customName === '' ? moldingMachineProps.id : moldingMachineProps.customName}</div>
-		<StatusTag class="flex-none" {...tagProps} />
-		<StatusLed class="flex-none" {...ledProps} />
-	</div>
-	<MachineHealthBar {...healthBarProps}/>
-	<div class="text-xs md:text-sm text-center">{$LL.machineHealthBar.legend()}</div>
-	<CyclePerformanceIndicator {...performanceIndicatorProps}/>
-	<div class="text-xs md:text-sm text-center">{$LL.performanceIndicator.legend()}</div>
-</div>
+<ContextMenu.Root>
+	<ContextMenu.Trigger>
+		<div
+			class="max-w-fit flex-col select-none cursor-default rounded-lg border bg-card text-card-foreground gap-4 m-2 p-4 shadow sm:w-[350px] md:w-[400px] md:shadow-lg lg:w-[450px]"
+		>
+			<div class="flex w-full gap-x-1 mb-4 h-auto">
+				<div class="flex-1 grow text-md uppercase">
+					{moldingMachineProps.customName === ''
+						? moldingMachineProps.id
+						: moldingMachineProps.customName}
+				</div>
+				<StatusTag class="flex-none" {...tagProps} />
+				<StatusLed class="flex-none" {...ledProps} />
+			</div>
+			<MachineHealthBar {...healthBarProps} />
+			<div class="text-xs md:text-sm text-center">{$LL.machineHealthBar.legend()}</div>
+			<CyclePerformanceIndicator {...performanceIndicatorProps} />
+			<div class="text-xs md:text-sm text-center">{$LL.performanceIndicator.legend()}</div>
+		</div>
+	</ContextMenu.Trigger>
+	<ContextMenu.Content>
+		<ContextMenu.Item>Open details</ContextMenu.Item>
+		<ContextMenu.Item>Cycle history</ContextMenu.Item>
+		<ContextMenu.Item>Rename Machine</ContextMenu.Item>
+		<ContextMenu.Item>Delete machine</ContextMenu.Item>
+	</ContextMenu.Content>
+</ContextMenu.Root>
