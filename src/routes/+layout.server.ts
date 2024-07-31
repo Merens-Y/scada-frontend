6e7eb31ab02b4edf4289aba43591846daa32fa7e
Merens-Y <58517637+Merens-Y@users.noreply.github.com>
2024-07-31 01:28:41 -0400

    testing cookies for language selection

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,33 +1,43 @@
 import type { LayoutServerLoad } from './$types';
-// import { detectLocale } from '$lib/i18n/i18n-util';
-// import { redirect } from '@sveltejs/kit';
+import { detectLocale } from '$lib/i18n/i18n-util';
+import { redirect } from '@sveltejs/kit';
 
 import { loadFlash } from 'sveltekit-flash-message/server';
 
 import { createBaseMetaTags } from '$lib/utils/metaTags';
 
-// const langParam = 'lang';
+const langParam = 'lang';
 
 // export const load = (async (event) => {
-//   // Using a GET var "lang" to change locale
-//   const newLocale = event.url.searchParams.get(langParam);
-//   if (newLocale) {
-//     event.cookies.set(langParam, newLocale, { path: '/' });
-//     event.url.searchParams.delete(langParam);
-//     // Redirect to remove the GET var    
-//     throw redirect(303, event.url.toString());
-//   }
+// 	// Using a GET var "lang" to change locale
+// 	const newLocale = event.url.searchParams.get(langParam);
+// 	if (newLocale) {
+// 		event.cookies.set(langParam, newLocale, { path: '/' });
+// 		event.url.searchParams.delete(langParam);
+// 		// Redirect to remove the GET var
+// 		throw redirect(303, event.url.toString());
+// 	}
 
-//   // Get the locale from the cookie
-//   const locale = detectLocale(() => [event.cookies.get(langParam) ?? '']);
-//   return { locale };
+// 	// Get the locale from the cookie
+// 	const locale = detectLocale(() => [event.cookies.get(langParam) ?? '']);
+// 	return { locale };
 // }) satisfies LayoutServerLoad;
 
-export const load = loadFlash(async ({ url, locals: { session } }) => {
+export const load = loadFlash(async ({ cookies, url, locals: { session } }) => {
 	const baseMetaTags = createBaseMetaTags(url);
-
+	// Using a GET var "lang" to change locale
+	const newLocale = url.searchParams.get(langParam);
+	if (newLocale) {
+		cookies.set(langParam, newLocale, { path: '/' });
+		url.searchParams.delete(langParam);
+		// Redirect to remove the GET var
+		throw redirect(303, url.toString());
+	}
+	// Get the locale from the cookie
+	const locale = detectLocale(() => [cookies.get(langParam) ?? '']);
 	return {
 		isUserLoggedIn: session !== null,
-		baseMetaTags: Object.freeze(baseMetaTags)
+		baseMetaTags: Object.freeze(baseMetaTags),
+		locale
 	};
 }) satisfies LayoutServerLoad;
